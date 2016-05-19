e393bc2f3f273c7784848d3645a7c2da8d2b5b32
Aubhro <aubrosengupta@gmail.com>
2016-05-18 19:25:32 -0700

    Initial Commit

---
+++ EasyQL/EasyQL.h
@@ -0,0 +1,13 @@
+//
+//  EasyQL.h
+//  EasyQL
+//
+//  Created by Aubhro Sengupta on 5/18/16.
+//  Copyright © 2016 Aubhro Sengupta. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface EasyQL : NSObject
+
+@end
---
+++ EasyQL/EasyQL.m
@@ -0,0 +1,13 @@
+//
+//  EasyQL.m
+//  EasyQL
+//
+//  Created by Aubhro Sengupta on 5/18/16.
+//  Copyright © 2016 Aubhro Sengupta. All rights reserved.
+//
+
+#import "EasyQL.h"
+
+@implementation EasyQL
+
+@end
